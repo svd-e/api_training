de938e8ba12348cf71a23f73aa0c6d08c20a3a94
Dmitriy.Sviridov <svdimitry@gmail.com>
2021-11-23 12:43:04 +0300

    Code improvements

--- methods.py
+++ methods.py
@@ -6,43 +6,27 @@ class MainMethods:
     def __init__(self):
         self.url = "https://api.openbrewerydb.org/breweries/"
 
-    def request_by_id(self, req):
-        self.id = req
+    def request_by_id(self, id):
+        self.id = id
         self.res = requests.get(self.url + self.id)
         self.data = self.res.json()
         return self.data
 
-    def request_sorting(self, req):
+    def request_sorting(self, params, sort_param):
         self.sorted_data = []
-        # print(self.sorted_data)
-
-        self.order = req
-        self.res = requests.get(self.url + self.order)
+        self.res = requests.get(self.url, params=params)
         self.data = self.res.json()
-        # print(json.dumps(self.data, indent=4))
         for i in range(0, len(self.data)):
-            self.sorted_data.append(self.data[i]["city"])
-            # print(self.data[i]["phone"])
-            # print(json.dumps(self.data[i], indent=4))
-            # print(self.data[i]["phone"])
-        # print(self.sorted_data)
-        # print(self.data)
-        # return self.sorted_data
+            self.sorted_data.append(self.data[i][sort_param])
+            # print(self.data[i][sort_param])
+        # print(json.dumps(self.data, indent=4))
 
     def is_the_order_ascending(self):
         # print(self.sorted_data)
         # print(sorted(self.sorted_data))
-        # # print(self.sorted_data.sort(reverse=True))
-        # print(self.sorted_data.sort())
-        # pass
         assert self.sorted_data == sorted(self.sorted_data), "The order is not ascending"
-        # assert self.sorted_data == self.sorted_data.sort(reverse=True), "The order is not ascending"
-
 
     def is_the_order_descending(self):
-        # print(self.sorted_data)
-        # print(sorted(self.sorted_data))
-        # pass
         assert self.sorted_data == sorted(self.sorted_data, reverse=True), "The order is not descending"
 
 
@@ -52,8 +36,8 @@ class MainMethods:
     def is_the_status_code_is_404(self):
         assert self.res.status_code == 404, "Status code is not 404"
 
-    def is_the_parsed_id_matches_the_response(self, id):
-        assert self.data["id"] == id, "API returned the wrong id"
+    def is_the_parsed_id_matches_the_response(self):
+        assert self.data["id"] == self.id, "API returned the wrong id"
 
     def is_the_length_of_answer_is_correct(self):
         assert len(self.data) == 17, "Data has a wrong structure"
--- test_me.py
+++ test_me.py
@@ -1,15 +1,16 @@
 import pytest
 
-from methods import MainMethods
 
 @pytest.mark.test_get_brewery
 class TestGetBrewery():
 
-    def test_simple_positive(self, client):
-        id = "10-56-brewing-company-knox"
+    @pytest.mark.parametrize('id', ["10-56-brewing-company-knox", "zwei-brewing-co-fort-collins"])
+
+    def test_simple_positive(self, client, id):
+        # id = "10-56-brewing-company-knox"
         client.request_by_id(id)
         client.is_the_status_code_is_200()
-        client.is_the_parsed_id_matches_the_response(id)
+        client.is_the_parsed_id_matches_the_response()
         client.is_the_length_of_answer_is_correct()
         client.is_there_an_non_null_name_field()
 
@@ -38,25 +39,27 @@ class TestGetBrewery():
         client.is_the_failure_message_is_correct()
 
 @pytest.mark.test_list_breweries
+@pytest.mark.parametrize('sort_param', ["city", "country"])
 class TestListBreweries():
-    def test_of_ascending_order(self, client):
-        req = "?sort=city:asc&per_page=50"
-        client.request_sorting(req)
+    def test_of_ascending_order(self, client, sort_param):
+        # req = "?sort=city:asc&per_page=50"
+        sort_type = "asc"
+        params = {"sort": f"{sort_param}:{sort_type}", "per_page": 50}
+        client.request_sorting(params, sort_param)
         client.is_the_status_code_is_200()
         client.is_the_order_ascending()
 
-    def test_of_descending_order(self, client):
-        req = "?sort=city:desc&per_page=50"
-        client.request_sorting(req)
+    def test_of_descending_order(self, client, sort_param):
+        # req = "?sort=city:desc&per_page=50"
+        sort_type = "desc"
+        params = {"sort": f"{sort_param}:{sort_type}", "per_page": 50}
+        client.request_sorting(params, sort_param)
         client.is_the_status_code_is_200()
         client.is_the_order_descending()
-
-
-
-
 
 
     # 10-barrel-brewing-co-denver-denver
 
     # pytest -v -s -m test_get_brewery test_me.py
     # pytest -v -s -m test_list_breweries test_me.py
+    # print(json.dumps(self.data, indent=4))
